c784812740fe98a77dd52c29e059ad3a53b618e2
Navarroo <37313645+Navarroo@users.noreply.github.com>
2018-05-17 22:39:16 -0300

    Add files via upload

--- Algoritimos 2 trim/algoritmos/aula15ATV.py
+++ Algoritimos 2 trim/algoritmos/aula15ATV.py
@@ -20,22 +20,46 @@
 
 # ====== M É D I A = P O N D E R A D A =====
 
-t = int(input("Lista: "))
+# t = int(input("Lista: "))
+#
+# lista = [""]* t
+# p = 0
+# while p < len(lista):
+#     lista[p] = int(input("Número: "))
+#     p+=1
+#
+#
+# soma = 0
+# pesos = 0
+# p = 0
+# while p < len(lista):
+#     soma = soma + (lista[p]*p)
+#     pesos = pesos + p
+#     p+=1
+#
+# medpon = soma / pesos
+# print ("Média ponderada: ", medpon)
 
-lista = [""]* t
-p = 0
-while p < len(lista):
-    lista[p] = int(input("Número: "))
-    p+=1
+# ====== C O N T A R = P A R E S = E = I M P A R E S ======
 
-
-soma = 0
-pesos = 0
-p = 0
-while p < len(lista):
-    soma = soma + (lista[p]*p)
-    pesos = pesos + p
-    p+=1
-
-medpon = soma / pesos
-print ("Média ponderada: ", medpon)
+# t = int(input("Lista: "))
+#
+# lista = [""]* t
+# p = 0
+# while p < len(lista):
+#     lista[p] = int(input("Número: "))
+#     p+=1
+#
+# par = 0
+# impar = 0
+# p = 0
+# while p < len(lista):
+#     if lista[p]%2 ==0:
+#         par = par + 1
+#     if lista[p]%2 != 0:
+#         impar = impar + 1
+#
+#     p+=1
+#
+# print("A quantidade de pares é: ", par)
+# print("A quantidade de Ímpares é: ", impar)
