cd88a8d7d2891014b80735c474280a5a22ad3707
Aluno <lucasnavarro301@gmail.com>
2018-06-07 21:49:34 -0300

    Aula Vetor

--- Algoritimos 2 trim/algoritmos/aula15.py
+++ Algoritimos 2 trim/algoritmos/aula15.py
@@ -1,70 +1,70 @@
-# #criando uma lista vazia
-# listaVazia = []
-#
-# #criando uma lista com 10 posições
-#
-# lista = [""]*10
-#
-# #Pedir tamanho da lista
-# t = int(input("Tamanho da lista"))
-#
-# lista = [""]*t
-#
-# lista = [""] * 5
-#
-# #preenchendo uma lista manualmente
-#
-# lista[0] = 13
-# lista[1] = 20
-# lista[2] = 34
-# lista[3] = 50
-# lista[4] = 21
-#
-# #mostrando valores
-#
-# print("Na primeira posição", lista[0])
-# print("Na segunda posição", lista[1])
-# print("Na terceira posição", lista[2])
-# print("Na quarta posição", lista[3])
-# print("Na quinta posição", lista[4])
-#
-# lista[3] = 29
-#
-#
-# lista = [""]*5
-# p = 0
-# while p <= 4:
-#     lista[p] = input("Número: ")
-#     lista[p] = int(lista[p])
-#     p = p + 1
-#
-# p = 0
-# while p < len(lista):
-#     print("Posição",p," - Valor: ",lista[p])
-#     p+=1
-# ----------------------------------------------
-# lista = [""]*5
-# p = 0
-# while p <= 4:
-#     lista[p] = input("Número: ")
-#     lista[p] = int(lista[p])
-#     p = p + 1
+#criando uma lista vazia
+listaVazia = []
 
-# p = len(lista) - 1
-# while p >= 0:
-#     print("Posição",p,"- Valor: ",lista[p])
-#     p = p - 1
-# ------------------------------------------------
-# ++++++++++++++++++++++++++++++++++++++++++++++++
-# ================================================
+#criando uma lista com 10 posições
 
-# t = int(input("Lista"))
-#
-# lista = [""]*t
-#
-# while p < len(lista) :
-#     lista[p] = int(input("Número")
-#     p+=1
+lista = [""]*10
+
+#Pedir tamanho da lista
+t = int(input("Tamanho da lista"))
+
+lista = [""]*t
+
+lista = [""] * 5
+
+#preenchendo uma lista manualmente
+
+lista[0] = 13
+lista[1] = 20
+lista[2] = 34
+lista[3] = 50
+lista[4] = 21
+
+#mostrando valores
+
+print("Na primeira posição", lista[0])
+print("Na segunda posição", lista[1])
+print("Na terceira posição", lista[2])
+print("Na quarta posição", lista[3])
+print("Na quinta posição", lista[4])
+
+lista[3] = 29
 
 
-# ====== S O M A ===================================
+lista = [""]*5
+p = 0
+while p <= 4:
+    lista[p] = input("Número: ")
+    lista[p] = int(lista[p])
+    p = p + 1
+
+p = 0
+while p < len(lista):
+    print("Posição",p," - Valor: ",lista[p])
+    p+=1
+----------------------------------------------
+lista = [""]*5
+p = 0
+while p <= 4:
+    lista[p] = input("Número: ")
+    lista[p] = int(lista[p])
+    p = p + 1
+
+p = len(lista) - 1
+while p >= 0:
+    print("Posição",p,"- Valor: ",lista[p])
+    p = p - 1
+------------------------------------------------
+++++++++++++++++++++++++++++++++++++++++++++++++
+================================================
+
+t = int(input("Lista"))
+
+lista = [""]*t
+
+while p < len(lista) :
+    lista[p] = int(input("Número")
+    p+=1
+
+
+====== S O M A ===================================
--- Algoritimos 2 trim/algoritmos/aula15ATV.py
+++ Algoritimos 2 trim/algoritmos/aula15ATV.py
@@ -1,65 +1,65 @@
-# ====== S O M A = E = M É D I A======
+====== S O M A = E = M É D I A======
 
-# t = int(input("Lista: "))
-#
-# lista = [""]* t
-# p = 0
-# while p < len(lista):
-#     lista[p] = int(input("Número: "))
-#     p+=1
-#
-# p = 0
-# soma = 0
-#
-# while p < len(lista):
-#     soma = soma + lista[p]
-#     p+=1
-#
-# print("A soma é",soma)
-# print("A média é", soma/len(lista))
+t = int(input("Lista: "))
 
-# ====== M É D I A = P O N D E R A D A =====
+lista = [""]* t
+p = 0
+while p < len(lista):
+    lista[p] = int(input("Número: "))
+    p+=1
 
-# t = int(input("Lista: "))
-#
-# lista = [""]* t
-# p = 0
-# while p < len(lista):
-#     lista[p] = int(input("Número: "))
-#     p+=1
-#
-#
-# soma = 0
-# pesos = 0
-# p = 0
-# while p < len(lista):
-#     soma = soma + (lista[p]*p)
-#     pesos = pesos + p
-#     p+=1
-#
-# medpon = soma / pesos
-# print ("Média ponderada: ", medpon)
+p = 0
+soma = 0
 
-# ====== C O N T A R = P A R E S = E = I M P A R E S ======
+while p < len(lista):
+    soma = soma + lista[p]
+    p+=1
 
-# t = int(input("Lista: "))
-#
-# lista = [""]* t
-# p = 0
-# while p < len(lista):
-#     lista[p] = int(input("Número: "))
-#     p+=1
-#
-# par = 0
-# impar = 0
-# p = 0
-# while p < len(lista):
-#     if lista[p]%2 ==0:
-#         par = par + 1
-#     if lista[p]%2 != 0:
-#         impar = impar + 1
-#
-#     p+=1
-#
-# print("A quantidade de pares é: ", par)
-# print("A quantidade de Ímpares é: ", impar)
+print("A soma é",soma)
+print("A média é", soma/len(lista))
+
+====== M É D I A = P O N D E R A D A =====
+
+t = int(input("Lista: "))
+
+lista = [""]* t
+p = 0
+while p < len(lista):
+    lista[p] = int(input("Número: "))
+    p+=1
+
+
+soma = 0
+pesos = 0
+p = 0
+while p < len(lista):
+    soma = soma + (lista[p]*p)
+    pesos = pesos + p
+    p+=1
+
+medpon = soma / pesos
+print ("Média ponderada: ", medpon)
+
+====== C O N T A R = P A R E S = E = I M P A R E S ======
+
+t = int(input("Lista: "))
+
+lista = [""]* t
+p = 0
+while p < len(lista):
+    lista[p] = int(input("Número: "))
+    p+=1
+
+par = 0
+impar = 0
+p = 0
+while p < len(lista):
+    if lista[p]%2 ==0:
+        par = par + 1
+    if lista[p]%2 != 0:
+        impar = impar + 1
+
+    p+=1
+
+print("A quantidade de pares é: ", par)
+print("A quantidade de Ímpares é: ", impar)
